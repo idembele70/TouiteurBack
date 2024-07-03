745ca8520d07f1a48057e7f9c8aa4459119ada3e
idembele70 <idembele70@gmail.com>
2024-07-03 21:09:58 +0200

    feat: #1 - add descriptive title for each subtest

---
+++ e2e/001_login.spec.ts
@@ -0,0 +1,16 @@
+import { test } from '@playwright/test';
+
+test.describe('It should test user register API Endpoints', async ()=> {
+
+  test('It should successfully register a new user', async ({request}) => {
+
+  })
+  
+  test('It should not allow registration with an existing email', async ({request}) => {
+
+  })
+
+  test('It should fail registering a new user', async ({request}) => {
+
+  })
+})
