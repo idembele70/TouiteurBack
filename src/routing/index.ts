09373ccca8a1017bc717f6f7ae7ad88873d9aa5b
idembele70 <idembele70@gmail.com>
2024-07-03 20:57:44 +0200

    feat : #1 - update the config file & fix web server running issue - store defineConfig inside a variable for easier to extension - decrease the sub-test timeout to 15 seconds - Run test files in serial - remove retries - set workers to one - add github reporter - define baseURL - Initialized project named 'local' - remove 'reuseExistingServer' option from webServer

--- src/routing/index.ts
+++ src/routing/index.ts
@@ -3,7 +3,7 @@ import touitesRouter from './touites.routes'
 import usersRouter from './users.routes'
 import authRouter from './auth.routes'
 const router = Router()
-
+router.get('', (_,res)=>res.send("Welcome home"))
 router.use("/touites", touitesRouter)
 router.use("/users", usersRouter)
 router.use("/auth", authRouter)
