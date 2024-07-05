524a0e8f9b8e54bcabe3fac5b1c76fb7b39f9453
idembele70 <idembele70@gmail.com>
2024-07-05 15:31:10 +0200

    chore: #1 - retrieve env variables from private repos when in CI/CD pipeline

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,8 +1,9 @@
 import mongoose from 'mongoose'
 
-const { MONGO_URL } = process.env
+const { MONGO_USERNAME, MONGO_PASSWORD, MONGO_CLUSTER, MONGO_OPTIONS, MONGO_APP_NAME } = process.env
+  const MONGODB_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_CLUSTER}/?${MONGO_OPTIONS}&appName=${MONGO_APP_NAME}`
 mongoose.connect(
-  MONGO_URL || ""
+  MONGODB_URL || ""
 ).then(
   () => console.log("Connected to db")
 )
--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -3,7 +3,11 @@ import { BinaryToTextEncoding } from "crypto";
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      MONGO_URL: string;
+      MONGO_USERNAME: string;
+      MONGO_PASSWORD: string;
+      MONGO_CLUSTER: string;
+      MONGO_OPTIONS: string;
+      MONGO_APP_NAME: string;
       PORT: number;
       PASSWORD_SECRET_KEY: string;
       JWT_SECRET_KEY: string;
