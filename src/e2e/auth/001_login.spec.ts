dbbb9b380c7ac8566001fae961da0e2cbad62d1e
idembele70 <idembele70@gmail.com>
2024-07-20 04:02:07 +0200

    feat: #5 - refactor & finish login test cases

--- src/e2e/auth/001_login.spec.ts
+++ src/e2e/auth/001_login.spec.ts
@@ -1,45 +1,174 @@
-import { APIRequestContext, expect, test } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 import { StatusCodes } from 'http-status-codes';
-import { LoggedUserProps, LoginProps, RegisterProps } from '../../controllers/auth.controllers';
+import { LoggedUserProps, login, LoginProps, RegisterProps } from '../../controllers/auth.controllers';
 import { deleteUser, register } from '../helper';
-// '@' is a convention to tag tests
 test.describe('It should test user login API endpoints', {
   tag: '@auth @login'
 } ,async () => {
   // The mode can be serial or parallel depending on the link between the sub-tests
   test.describe.configure({mode:'serial'})
-
-  // Your global variables
+  const OneMillisecond = 1000
   const userCredentials: RegisterProps = {
     email: "test_setup_email@invalid.invalid",
     username: "test_setup_username",
     password: "test_setup_password"
   }
+  const loginWithUsernameCredentials: LoginProps = {
+    username: "test_setup_username",
+    password: "test_setup_password"
+  }
+  const loginWithEmailCredentials: LoginProps = {
+    email: "test_setup_email@invalid.invalid",
+    password: "test_setup_password"
+  }
 
-  // Add your setup data 
-  test.beforeAll( async({ request })=> {
-    await register(request, userCredentials)
+
+  // Add your setup data
+  test.beforeAll( async( )=> {
+    await register(userCredentials)
   })
-  // Clean your setup data 
-  test.afterAll( async({request})=> {
-    await deleteUser(request, userCredentials.username)
+  // Clean your setup data
+  test.afterAll( async()=> {
+    await deleteUser(userCredentials.username)
   })
 
-  test('It should sucessfully login a user', async ({request}) => {
-    const loginCredentials: LoginProps = {...userCredentials}
-    delete loginCredentials.username
-    const OneMillisecond = 1000
-    const timestampBeforeLogininSecond = Date.now() / OneMillisecond
+  test('It should sucessfully login a user using email', async ({ request }) => {
+    const timestampBeforeLoginInSecond = Date.now() / OneMillisecond
     const response = await request.post('auth/login', {
-      data: loginCredentials
+      data: loginWithEmailCredentials
     })
+
     expect(response.status()).toEqual(StatusCodes.OK)
     const responseJson = await response.json() as LoggedUserProps
-    expect(responseJson.exp).toBeGreaterThan(timestampBeforeLogininSecond)
+    expect(responseJson.exp).toBeGreaterThan(timestampBeforeLoginInSecond)
     expect(responseJson).toEqual(expect.objectContaining({
       email: userCredentials.email,
       username: userCredentials.username,
       isAdmin:false,
     }))
+  })
+
+  test('It should sucessfully login a user using username', async ({ request }) => {
+    const timestampBeforeLoginInSecond = Date.now() / OneMillisecond
+    const response = await request.post('auth/login', {
+      data: loginWithUsernameCredentials
+    })
+    expect(response.status()).toEqual(StatusCodes.OK)
+    const responseJson = await response.json() as LoggedUserProps
+    expect(responseJson.exp).toBeGreaterThan(timestampBeforeLoginInSecond)
+    expect(responseJson).toEqual(expect.objectContaining({
+      email: userCredentials.email,
+      username: userCredentials.username,
+      isAdmin:false,
+    }))
+  })
+
+  test('It should fail to login a user with an incorrect password', async ({ request }) => {
+    const response = await request.post('auth/login', {
+      data: {
+        email: loginWithEmailCredentials.email,
+        password: "incorrect_Password"
+      }
+    })
+    expect(response.status()).toEqual(StatusCodes.FORBIDDEN)
+    const responseJson = await response.json() as LoggedUserProps
+    expect(responseJson).toEqual({
+      error: 'Invalid password',
+    })
+  })
+
+  test('It should fail to login a user with providing only username', async ({ request }) => {
+    const response = await request.post('auth/login', {
+      data: {
+        username: loginWithUsernameCredentials.username
+      }
+    })
+    expect(response.status()).toEqual(StatusCodes.BAD_REQUEST)
+    const responseJson = await response.json() as LoggedUserProps
+    expect(responseJson).toEqual({
+      error: 'Password is required',
+    })
+  })
+
+  test('It should fail to login a user with providing only email', async ({ request }) => {
+    const response = await request.post('auth/login', {
+      data: {
+        email: loginWithEmailCredentials.email
+      }
+    })
+    expect(response.status()).toEqual(StatusCodes.BAD_REQUEST)
+    const responseJson = await response.json() as LoggedUserProps
+    expect(responseJson).toEqual({
+      error: 'Password is required',
+    })
+  })
+
+  test('It should fail to login a user with providing only password', async ({ request }) => {
+    const response = await request.post('auth/login', {
+      data: {
+        password: loginWithEmailCredentials.password
+      }
+    })
+    expect(response.status()).toEqual(StatusCodes.BAD_REQUEST)
+    const responseJson = await response.json() as LoggedUserProps
+    expect(responseJson).toEqual({
+      error: 'Email or username is required',
+    })
+    })
+
+  test('It should fail to login a username that doesn\'t exist', async ({ request }) => {
+    const response = await request.post('auth/login', {
+      data: {
+        username: 'unexisting_user',
+        password: loginWithEmailCredentials.password
+      }
+    })
+    expect(response.status()).toEqual(StatusCodes.BAD_REQUEST)
+    const responseJson = await response.json() as LoggedUserProps
+    expect(responseJson).toEqual({
+      error: 'A user with that username doesn\'t exists',
+    })
+    })
+  
+  test('It should fail to login a email that doesn\'t exist', async ({ request }) => {
+    const response = await request.post('auth/login', {
+      data: {
+        email: 'existing_user@invalid.invalid',
+        password: loginWithEmailCredentials.password
+      }
+    })
+    expect(response.status()).toEqual(StatusCodes.BAD_REQUEST)
+    const responseJson = await response.json() as LoggedUserProps
+    expect(responseJson).toEqual({
+      error: 'A user with that email doesn\'t exists',
+    })
+    })
+  
+  test('It should fail to login a user using a good username but a wrong password', async ({request})=> {
+    const response = await request.post('auth/login', {
+      data: {
+        username: loginWithUsernameCredentials.username,
+        password: 'invalid_password_125478'
+      }
+    })
+    expect(response.status()).toEqual(StatusCodes.FORBIDDEN)
+    const responseJson = await response.json() as LoggedUserProps
+    expect(responseJson).toEqual({
+      error: 'Invalid password',
+    })
+  })
+
+  test('It should fail to login a user using a good email but a wrong password', async ({request})=> {
+    const response = await request.post('auth/login', {
+      data: {
+        email: loginWithEmailCredentials.email,
+        password: 'invalid_password_125478'
+      }
+    })
+    expect(response.status()).toEqual(StatusCodes.FORBIDDEN)
+    const responseJson = await response.json() as LoggedUserProps
+    expect(responseJson).toEqual({
+      error: 'Invalid password',
+    })
   })
 })
